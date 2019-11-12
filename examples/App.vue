e58d615f03bc94f2487b2d815dd8a0d9a04be210
sn_baby <sn_baby@qq.com>
2019-11-12 14:19:29 +0800

    修改

---
+++ examples/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <colorPicker v-model="color" v-on:change="headleChangeColor"></colorPicker>
+        <img alt="Vue logo1" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        },
+        data() {
+            return {
+                color: '#ff0000'
+            }
+        },
+        methods: {
+            headleChangeColor() {
+                console.log('颜色改变')
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
